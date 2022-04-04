43b49863a86586a6e72f71b0126b984f26d587d6
Lucas Zhou <lucaszhou@Lucass-MacBook-Pro.local>
2022-04-04 01:55:00 -0400

    second commit:

---
+++ js/results.js
@@ -0,0 +1,39 @@
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
